7a358fde7a56b66da9f7bd60e7eb5814021c630e
Mark Murtagh <mamurtag@tcd.ie>
2017-05-18 15:09:02 +0100

    very basic working program

--- scrapey.py
+++ scrapey.py
@@ -13,7 +13,6 @@ homeOdd = {}						#dictionary containing all home odds + bookie
 drawOdd = {}						#dictionary containing all draw odds + bookie
 awayOdd = {}						#dictionary containing all away odds + bookie
 
-
 #Changes string representation of odds into decimal odds
 def sFracToFloat(fracStr):
 	if '/' in fracStr:
@@ -30,11 +29,29 @@ def percentage(oddFloat):
 
 # Calcualte whether the created market is over/under arb, and if so....
 # ... return percentage
-def total(createdMarketList):
-	home = createdMarketList[0]
-	draw = createdMarketList[1]
-	away = createdMarketList[2]
-	return home+draw+away
+def total(h, d, a):
+	return h+d+a
+
+#runs through all combinations of home, draw, away, and extracts all....
+#... combinations which are arbs, puts them in dict and returns dict.
+def extractArbs(homes, draws, aways):
+	arb = {}
+	arbs = {}
+	count=0
+	for x in homes:
+		for y in draws:
+			for z in aways:
+				h=percentage(sFracToFloat(homes[x]))
+				d=percentage(sFracToFloat(draws[y]))
+				a=percentage(sFracToFloat(aways[z]))
+				#print total(h,d,a)
+				if total(h,d,a)<100:
+					arb[x]=h
+					arb[y]=d
+					arb[z]=a
+					arbs[count] = arb
+					count+=1
+
 
 for i in range(2, 10):
 	home = '//*[@id="t1"]/tr[1]/td[' + str(i) +']/text()'
@@ -48,10 +65,12 @@ for i in range(2, 10):
 	homeOdd[bookieName[0]]=homeTeam[0]								
 	drawOdd[bookieName[0]]=drawTeam[0]								
 	awayOdd[bookieName[0]]=awayTeam[0]
-	for x in homeOdd:
-		testList = percentage(sFracToFloat(homeOdd[x]))
-		print x, homeOdd[x], drawOdd[x], awayOdd[x] 
-		print x, 'H:', homeOdd[x], 'D:', drawOdd[x], 'A:', awayOdd[x], percentage(sFracToFloat(homeOdd[x])), percentage(sFracToFloat(drawOdd[x])), percentage(sFracToFloat(awayOdd[x])) 							
+
+for x in homeOdd:
+	# 	testList = percentage(sFracToFloat(homeOdd[x]))
+	# 	print x, homeOdd[x], drawOdd[x], awayOdd[x] 
+	# 	print x, 'H:', homeOdd[x], 'D:', drawOdd[x], 'A:', awayOdd[x], percentage(sFracToFloat(homeOdd[x])), percentage(sFracToFloat(drawOdd[x])), percentage(sFracToFloat(awayOdd[x]))
+	myArbDictionary = extractArbs(homeOdd, drawOdd, awayOdd)
 
 
 
