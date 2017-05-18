d4e7723c085b981ff2c15bc8f98107d3c10b9fe7
Mark Murtagh <mamurtag@tcd.ie>
2017-05-18 14:27:07 +0100

    Actually doing soemthing with data

--- scrapey.py
+++ scrapey.py
@@ -9,9 +9,21 @@ tree = html.fromstring(page.content)
 
 homeTeamName = tree.xpath('//*[@id="t1"]/tr[1]/td[1]/span[1]/@data-name')
 awayName = tree.xpath('//*[@id="t1"]/tr[3]/td[1]/span[1]/@data-name')
-homeOdd = {}
-drawOdd = {}
-awayOdd = {}
+homeOdd = {}						#dictionary containing all home odds + bookie
+drawOdd = {}						#dictionary containing all draw odds + bookie
+awayOdd = {}						#dictionary containing all away odds + bookie
+
+
+#Changes string representation of odds into decimal odds
+def sFracToFloat(fracStr):
+	if '/' in fracStr:
+		numbers = fracStr.split("/")
+		decimal= (float(numbers[0])/float(numbers[1])) + 1
+		return decimal
+	else:
+		dec = float(fracStr) + 1
+		return dec
+
 
 for i in range(2, 10):
 	home = '//*[@id="t1"]/tr[1]/td[' + str(i) +']/text()'
@@ -22,9 +34,20 @@ for i in range(2, 10):
 	drawTeam = tree.xpath(draw)
 	awayTeam = tree.xpath(away)
 	bookieName = tree.xpath(bookie)
-	homeOdd[bookieName[0]]=homeTeam[0]
-	drawOdd[bookieName[0]]=drawTeam[0]
+	homeOdd[bookieName[0]]=homeTeam[0]								
+	drawOdd[bookieName[0]]=drawTeam[0]								
 	awayOdd[bookieName[0]]=awayTeam[0]
+	for x in homeOdd:
+		print x, homeOdd[x], sFracToFloat(homeOdd[x])								
+
+
+
+
+#def calculateArb(homeOdd, drawOdd,awayOdd):
+#	for i in homeOdd:
+#		homePer = sFracToInt(homeOdd[i])
+
+
 
 
 #bookie 
